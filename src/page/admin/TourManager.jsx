6b9065796771d15644ed5df69402bbf553206c46
Đông <lamvandong12.11@gmail.com>
2026-03-30 20:42:41 +0700

    create tour management page

--- src/page/admin/TourManager.jsx
+++ src/page/admin/TourManager.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const TourManager = () => {
   return (
-    <div>TourManager</div>
+    <div className='bg-red-500'>TourManager
+      <h1>lam van dong</h1>
+    </div>
   )
 }
